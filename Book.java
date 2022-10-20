93e10a8ff6573be70d1dfeddf37cfc8db469b2e1
Arbaj Sheikh <112559813+jd-arbaj@users.noreply.github.com>
2022-10-20 13:46:12 +0530

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,3 +1,17 @@
+/*
+Q.3     Create a class Book with id, name as the member variables.
+
+	Create  BookNotFoundException as a exception and create one argument constructor which is accepting String message.
+
+	Create a test class with an array of 5 books with ids starting from 1.
+
+	Create a method findById(Book[] books , int id) which will take the previously created book array and one id to find the book.
+
+	Print "Found" if the book is present with the given id else throw BookNotFoundException with a message "Book with the given id is not present".
+
+	Handle the exception in main method and print the stacktrace in catch block.
+*/
+
 package com.exception.bll;
 
 public class Book {
