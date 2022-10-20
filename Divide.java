6d42ae5a8357bbcc551dacb6212824b8b125ba59
Arbaj Sheikh <112559813+jd-arbaj@users.noreply.github.com>
2022-10-20 13:43:24 +0530

    Update Divide.java

--- Divide.java
+++ Divide.java
@@ -1,3 +1,20 @@
+    /*
+    1.The professor you TA (Training Associate) for, Professor Jackson, shared with you the code she uses to auto-grade students’ work. She and the other TAs have encountered some problems with the code in the past when they enter the total possible point value for an assignment. Occasionally, they accidentally enter 0 for the total number of possible points and the program encounters a fatal error when trying to divide by 0.
+
+	To help out with this issue, complete a function called divide() in Main. 
+
+	The divide() method takes in two parameters: x and y.
+
+	Your function should return the result of x/y.
+
+	However, if y is zero, you should throw an exception. 
+
+	Try to use an ArithmeticException and put your try/catch block in divide() to test out your error-handling skills. 
+
+	If an exception is caught, make sure to print out a helpful message.
+*/
+
+
 package com.exception.bll;
 
 public class Divide {
