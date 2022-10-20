ff7c09ed718bc3d77f3af30f554d1b59568521bc
Arbaj Sheikh <112559813+jd-arbaj@users.noreply.github.com>
2022-10-20 11:45:45 +0530

    Add files via upload

---
+++ Divide.java
@@ -0,0 +1,32 @@
+package com.exception.bll;
+
+public class Divide {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Devision : "+divide(3,0));
+		
+	}
+
+	public static int divide(int x, int y) {
+		
+		int d=0;
+		try {
+			d = x/y;
+			
+	}
+		catch(ArithmeticException e) {
+			
+			 System.out.println("Can not divide by zero");
+	}
+		catch(Exception e) {
+			
+			System.out.println(e.getMessage());
+	}
+		
+		return d;
+	}
+
+}
+
+
